5124a5727acaa9f31e01d4dfd97890db0b1f850b
dawei <dawei.xing@dianping.com>
2013-12-28 21:07:09 +0800

    do something cool with RAC

--- nuexperiment/nuexperiment/NUExperimentViewController.m
+++ nuexperiment/nuexperiment/NUExperimentViewController.m
@@ -7,16 +7,99 @@
 //
 
 #import "NUExperimentViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
-@interface NUExperimentViewController ()
+typedef void(^TestAnimationBlock)(void);
+
+@interface UIView (nu_block_api)
++ (RACSignal *)nu_animateWithDuration:(NSTimeInterval)duration delay:(NSTimeInterval)delay options:(UIViewAnimationOptions)options animations:(void (^)(void))animations;
++ (RACSignal *)nu_animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations; // delay = 0.0, options = 0
+@end
+
+@implementation UIView (nu_block_api)
++ (RACSignal *)nu_animateWithDuration:(NSTimeInterval)duration delay:(NSTimeInterval)delay options:(UIViewAnimationOptions)options animations:(void (^)(void))animations {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [self animateWithDuration:duration delay:delay options:options animations:animations completion:^(BOOL finished) {
+            [subscriber sendNext:@(finished)];
+            [subscriber sendCompleted];
+        }];
+        return nil;
+    }];
+}
+
++ (RACSignal *) nu_animateWithDuration:(NSTimeInterval)duration animations:(void (^)(void))animations {
+    return [self nu_animateWithDuration:duration delay:0 options:0 animations:animations];
+}
 
 @end
 
+@interface NUExperimentViewController ()
+@property (nonatomic, strong) UIView *testview;
+@end
+
+
 @implementation NUExperimentViewController
+
+- (void) loadView {
+    [super loadView];
+    self.testview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    self.testview.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:self.testview];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (RACSignal *) animateWithDuration:(NSTimeInterval) interval animations:(TestAnimationBlock) animation {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [UIView animateWithDuration:interval animations:^{
+            animation();
+        } completion:^(BOOL finished) {
+            [subscriber sendNext:@(finished)];
+            [subscriber sendCompleted];
+        }];
+        return nil;
+    }];
+}
+
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+//    [[[[[[[self animateWithDuration:.7 animations:^{
+//        self.testview.frame = CGRectMake(0, 100, 50, 50);
+//    }] flattenMap:^RACStream *(id value) {
+//        return [self animateWithDuration:.7 animations:^{
+//            self.testview.frame = CGRectMake(100, 100, 50, 50);
+//        }];
+//    }] flattenMap:^RACStream *(id value) {
+//        return [self animateWithDuration:.7 animations:^{
+//            self.testview.frame = CGRectMake(100, 0, 50, 50);
+//        }];
+//    }] flattenMap:^RACStream *(id value) {
+//        return [self animateWithDuration:.7 animations:^{
+//            self.testview.frame = CGRectMake(0, 0, 50, 50);
+//        }];
+//    }] repeat] take:100] subscribeCompleted:^{
+//    }];
+    
+    [[[[[[[UIView nu_animateWithDuration:.7 animations:^{
+        self.testview.frame = CGRectMake(0, 100, 50, 50);
+    }] flattenMap:^RACStream *(id value) {
+        return [UIView nu_animateWithDuration:.6 animations:^{
+            self.testview.frame = CGRectMake(100, 100, 50, 50);
+        }];
+    }] flattenMap:^RACStream *(id value) {
+        return [UIView nu_animateWithDuration:.5 animations:^{
+            self.testview.frame = CGRectMake(100, 0, 50, 50);
+        }];
+    }] flattenMap:^RACStream *(id value) {
+        return [UIView nu_animateWithDuration:.4 animations:^{
+            self.testview.frame = CGRectMake(0, 0, 50, 50);
+        }];
+    }] repeat] take:100] subscribeCompleted:^{
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
