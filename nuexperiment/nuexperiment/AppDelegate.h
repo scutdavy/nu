1f0ebaf5cc4b01a94c99c911564e2ff2699fcf64
dawei <dawei.xing@dianping.com>
2013-12-28 15:50:47 +0800

    initial experiment project

---
+++ nuexperiment/nuexperiment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nuexperiment
+//
+//  Created by dawei on 12/28/13.
+//  Copyright (c) 2013 dawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nuexperiment/nuexperimentTests/nuexperimentTests.m
@@ -0,0 +1,30 @@
+//
+//  nuexperimentTests.m
+//  nuexperimentTests
+//
+//  Created by dawei on 12/28/13.
+//  Copyright (c) 2013 dawei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface nuexperimentTests : XCTestCase
+
+@end
+
+@implementation nuexperimentTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+}
+
+@end
